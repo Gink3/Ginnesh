0152a1b0c2cad1c1bbd59df86aded94c0de6bc24
gink3 <telltaylor@yahoo.com>
2020-04-15 20:53:14 -0500

    Added actor class

---
+++ source/actor.cpp
@@ -0,0 +1,32 @@
+#include"actor.h"
+
+void Actor::move(Actor& actor, int choice) {
+    
+
+}
+
+string Actor::GetName() {
+    return name;
+}
+int Actor::GetHealth() {
+    return health;
+}
+int Actor::GetX() {
+    return x;
+}
+int Actor::GetY() {
+    return y;
+}
+
+void Actor::SetName(string n) {
+    name = n;
+}
+void Actor::SetHealth(int h) {
+    health = h;
+}
+void Actor::SetX(int xpos) {
+    x = xpos;
+}
+void Actor::SetY(int ypos) {
+    y = ypos;
+}
--- source/actor.h
+++ source/actor.h
@@ -1,7 +1,32 @@
 #ifndef ACTOR_H
 #define ACTOR_H
+#include<string>
+
+using namespace std;
 
 
+class Actor {
+    private:
+        string name;
+        int health;
+        int x;
+        int y;
+    public:
+        string GetName();
+        int GetHealth();
+        int GetX();
+        int GetY();
+
+        void SetName(string n);
+        void SetHealth(int h);
+        void SetX(int xpos);
+        void SetY(int ypos);
+
+
+        void move(Actor& actor, int choice);
+        
+
+};
 
 
 
